3275878b9ab4b36e2db9547f3cac826603c7d13f
Richard Troester <richard.troester@eblusolutions.com>
2022-01-27 01:26:10 -0500

    updates for system changes

--- mm3e.js
+++ mm3e.js
@@ -1,7 +1,10 @@
+import {mm3e} from "./module/config.js"
 import MM3eItemSheet from "./module/sheets/MM3eItemSheet.js";
 
 Hooks.once("init", function() {
     console.log("M&M3e | Initializing Mutants & Masterminds 3e System | Shits about to get wild! Buckle up Buckaroo's!");
+
+    CONFIG.mm3e = mm3e;
 
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("mm3e", MM3eItemSheet, {makeDefault: true});
--- module/sheets/MM3eItemSheet.js
+++ module/sheets/MM3eItemSheet.js
@@ -3,4 +3,17 @@ export default class MM3eItemSheet extends ItemSheet {
         const path = "systems/MM3e-Foundry-VTT/templates/sheets/";
         return `${path}/${this.item.data.type}.html`;
     }
+
+    const baseData = super.getData();
+
+    let sheetData = {
+        owner: this.item.isOwnwer,
+        editable: this.isEditable,
+        item: baseData.item,
+        data: baseData.item.data.data,
+        config: CONFIG.mm3e
+    };
+
+    return sheetData;
+
 }
