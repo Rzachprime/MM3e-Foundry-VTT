82d47a51da1972b98c72bdac1cfd9aabf82d45d7
Richard Troester <richard.troester@eblusolutions.com>
2022-01-26 22:42:29 -0500

    fix for using ' instead of using `

--- module/sheets/MM3eItemSheet.js
+++ module/sheets/MM3eItemSheet.js
@@ -1,5 +1,5 @@
 export default class MM3eItemSheet extends ItemSheet {
     get template() {
-        return 'systems/MM3e-Foundry-VTT/templates/sheets'
+        return `systems/MM3e-Foundry-VTT/templates/sheets/${this.data.type}-sheet.html`;
     }
 }
