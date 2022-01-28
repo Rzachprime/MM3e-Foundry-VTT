766b834ab9ae14e338cdbc7e71c1bc08225c5ef6
Richard Troester <richard.troester@eblusolutions.com>
2022-01-27 22:44:55 -0500

    updated models and names

--- module/config.js
+++ module/config.js
@@ -6,3 +6,17 @@ mm3e.typeNames = {
     extra: "mm3e.type.extra",
     flaw: "mm3e.type.flaw"
 }
+
+mm3e.effectNames = {
+    type:"mm3e.effect.type",
+    name:"mm3e.effect.name",
+    effectDescription:"mm3e.effect.effectDescription",
+    cost: "mm3.effect.cost",
+    isFlat: "mm3e.effect.isFlat"
+}
+
+mm3e.advantageNames={
+    name:"mm3e.advantage.name",
+    collectionName:"mm3e.advantage.collectionName",
+    description:"mm3e.advanatage.description"
+}
--- module/sheets/MM3eItemSheet.js
+++ module/sheets/MM3eItemSheet.js
@@ -2,18 +2,19 @@ export default class MM3eItemSheet extends ItemSheet {
     get template() {
         const path = "systems/MM3e-Foundry-VTT/templates/sheets/";
         return `${path}/${this.item.data.type}.html`;
-    }
-
-    const baseData = super.getData();
-
-    let sheetData = {
-        owner: this.item.isOwnwer,
-        editable: this.isEditable,
-        item: baseData.item,
-        data: baseData.item.data.data,
-        config: CONFIG.mm3e
     };
 
-    return sheetData;
+    getData() {
+        var baseData = super.getData();
 
+        let sheetData = {
+            owner: this.item.isOwnwer,
+            editable: this.isEditable,
+            item: baseData.item,
+            data: baseData.item.data.data,
+            config: CONFIG.mm3e
+        };
+
+        return sheetData;
+    }
 }
