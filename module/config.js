bcd5727fa56e80e64392ac055e1d7dd688f649dd
Richard Troester <richard.troester@eblusolutions.com>
2022-01-28 00:47:05 -0500

    updated item sheets and configs

--- module/config.js
+++ module/config.js
@@ -18,5 +18,31 @@ mm3e.effectNames = {
 mm3e.advantageNames={
     name:"mm3e.advantage.name",
     collectionName:"mm3e.advantage.collectionName",
-    description:"mm3e.advanatage.description"
+    ruleName: "mm3e.advantage.ruleName",
+    description:"mm3e.advanatage.description",
+    type:"mm3e.advantage.type",
+    effectDescription:"mm3e.advantage.effectDescription",
+    cost: "mm3e.advantage.cost",
+    isFlat: "mm3e.advantage.isFlat"
+}
+
+mm3e.powerNames={
+    name:"mm3e.power.name",
+    groupName:"mm3e.power.groupName",
+    powerEffect:"mm3e.power.powerEffect",
+    description:"mm3e.power.description",
+    type:"mm3e.power.type",
+    effectDescription:"mm3e.power.effectDescription",  
+    cost: "mm3e.power.cost",
+    isFlat: "mm3e.power.isFlat"
+},
+mm3e.modifierNames={
+    name:"mm3e.modifier.name",
+    parentName:"mm3e.modifier.parentName",
+    groupName:"mm3e.modifier.groupName",
+    description:"mm3e.modifier.description",
+    type:"mm3e.modifier.type",
+    effectDescription:"mm3e.modifier.effectDescription",
+    cost: "mm3e.modifier.cost",
+    isFlat: "mm3e.modifier.isFlat"
 }
